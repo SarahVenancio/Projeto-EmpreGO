2d86e405586c4cee760e03f3df7cfad7524ef95e
SarahVenancio <sarah.venancio.senaisp@gmail.com>
2024-12-09 12:11:45 -0300

    alteração config

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ if ambiente == 'desenvolvimento':
     DB_PASSWORD = 'senai'
     DB_NAME = 'emprego'
 
+if ambiente == 'producao':
+    #CONFIG BANCO DE DADOS
+    DB_HOST = 'SarahVenancio.mysql.pythonanywhere-services.com'
+    DB_USER = 'SarahVenancio'
+    DB_PASSWORD = 'meSiv@9184'
+    DB_NAME = 'SarahVenancio$blog'
+
 #Chave secreta (session)
 SECRET_KEY = 'emprego'
 
